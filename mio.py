29ed29adedd2d0a14fa0bcbf4f360b604937ea1d
repl.it user <replituser@example.com>
2020-12-16 14:29:45 +0000

    due iterazioni stesso risultato

--- mio.py
+++ mio.py
@@ -1,1 +1,19 @@
 print("ciao")
+print("/// SOMMA DI UNA LISTA ///")
+listaz = [4,5,4,5,4,5,4,5]
+def sommaLista (lista):
+    sum=0
+    for item in lista:
+        sum = item + sum
+    print ("somma: {}".format(sum))
+sommaLista(listaz)
+
+
+print("/// SOMMA LISTA CON RETURN ///")
+def sommaReturn (list):
+    sum = 0
+    for item in list:
+        sum = sum + item
+    return sum
+
+print("la somma è: {}".format(sommaReturn(listaz)))
